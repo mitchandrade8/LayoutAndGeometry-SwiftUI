a5c0832e8ff0ec40349f4ead0423395d012d9788
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-04-09 21:22:40 -0600

    first commit

---
+++ LayoutAndGeometry/Practice/AlignmentAndAlignmentGuides.swift
@@ -0,0 +1,20 @@
+//
+//  AlignmentAndAlignmentGuides.swift
+//  LayoutAndGeometry
+//
+//  Created by Mitch Andrade on 4/9/22.
+//
+
+import SwiftUI
+
+struct AlignmentAndAlignmentGuides: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AlignmentAndAlignmentGuides_Previews: PreviewProvider {
+    static var previews: some View {
+        AlignmentAndAlignmentGuides()
+    }
+}
---
+++ LayoutAndGeometry/Practice/HowLayoutWorksInSwiftUI.swift
@@ -0,0 +1,20 @@
+//
+//  HowLayoutWorksInSwiftUI.swift
+//  LayoutAndGeometry
+//
+//  Created by Mitch Andrade on 4/9/22.
+//
+
+import SwiftUI
+
+struct HowLayoutWorksInSwiftUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HowLayoutWorksInSwiftUI_Previews: PreviewProvider {
+    static var previews: some View {
+        HowLayoutWorksInSwiftUI()
+    }
+}
