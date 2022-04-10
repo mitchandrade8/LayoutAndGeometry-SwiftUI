c00d66fc4a3fae76bac97f5ad706e9bbce86a513
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-04-09 20:54:16 -0600

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Mitch Andrade on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
